171916e8473868cc3ed8b3423ec37aaa5f77df77
493645853 <you@example.co & 'c:\Users\86180\.vscode\extensions\ms-vscode.cpptools-1.6.0\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-ixo2nlhm.xgk' '--stdout=Microsoft-MIEngine-Out-difmocxt.r1c' '--stderr=Microsoft-MIEngine-Error-u2emdc0m.l4s' '--pid=Microsoft-MIEngine-Pid-z3mhukb5.boe' '--dbgExe=D:\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin\gdb.exe' '--interpreter=mi' m>
2021-09-05 03:09:26 +0100

    Learn CMake

---
+++ Cpp/Basics/STL/learn_vector.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+/**
+ * @brief Main loop
+ * 
+ * @return int 
+ */
+int main(void)
+{
+    /**
+     * @brief Initialization of vector
+     * 
+     */
+    // 1. 
+    std::vector<int> vec_int{1,2,3};
+    std::vector<int> vec_int2 = {3,4,5};
+    std::vector<int> vec_int3;
+
+    /**
+     * @brief search
+     * 
+     */
+    // 1. conventional loop
+    std::cout<<"Conventional for loop:"<<std::endl;
+    for(int i = 0;i<vec_int.size();i++)
+    {
+        std::cout<<vec_int[i]<<std::endl;
+    }
+
+    // 2. iterator
+    for(auto i=vec_int.cbegin();i!=vec_int.cend();i++)
+    {
+        std::cout<<*i<<std::endl;
+    }
+
+    /**
+     * @brief Avoid termination
+     * 
+     */
+    while(1){}
+    return 0;
+}
